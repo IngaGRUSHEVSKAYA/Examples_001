d04b6bc865aec90c6fcdfe710e0ae828681c8da7
inga <inga.grushevskaya@mail.ru>
2023-02-01 19:29:25 +0300

    add Examples002 HelloUser

---
+++ Examples002_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+*/
+
+Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
